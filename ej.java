55dac2829e6b7f7802f65791f5a00a8a6bfe1b11
Marco Berrio <102123641+Runari0@users.noreply.github.com>
2022-05-08 14:05:06 -0500

    Add files via upload

---
+++ ej.java
@@ -0,0 +1,32 @@
+//? 1.Crea una clase llamada VarTest
+//? 2.Crea las siguientes 3 variables no inicializadas
+    //? - int iNum;
+    //? -char cVal;
+    //? -boolean bVal;
+//? 3.mostrar todos los valores a la consola
+//? 4. actualice su codigo para asignar lo siguiente:
+    //? -iNum -25
+    //? -char -b
+    //? -boolean bVal -true
+//? 5.mostar todos los valores a la consola
+
+package vartest;
+public class VarTest{//? 1
+    public static void (String[] args) {
+        int iNum;//? 2
+        char cVal;
+        boolean bVal;
+        //?mostar 3
+        system.out.println(iNum);
+        system.out.println(cVal);
+        system.out.println(bVal);
+
+        int iNum = 25;//? 4
+        char cVal = B;
+        boolean bVal = true;
+        //?mostar
+        system.out.println(iNum); //? 5
+        system.out.println(cVal);
+        system.out.println(bVal);
+    }
+}
